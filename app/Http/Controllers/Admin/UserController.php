d60b6a165e5b1405f6da73043f5845e760c8b458
fatuma.mkima <fettymkima@gmail.com>
2018-09-27 07:35:47 +0300

    role and role profile changes

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -95,7 +95,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,25 @@ use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
 
-    $user =  User::orderBy('id', 'desc')->first();
-
-    return $user;
+    $roleProfiles = DB::table('role_profiles')
+        ->join('roles', 'role_profiles.role_id', '=', 'roles.id')
+        ->select('roles.role_name','role_profiles.id')
+        ->where('role_profiles.profile_id', '=', 3)
+        ->get();
+    return $roleProfiles;
 });
 
+//user route
 Route::get('user/register','Admin\UserController@create');
 Route::resource('user','Admin\UserController');
 
+//Role route
+Route::get('role/All','Admin\RoleController@getAllRole');
+Route::resource('role','Admin\RoleController');
+Route::post('role/update','Admin\RoleController@updates');
+
+//Role Profile
+Route::get('roleProfile/assign/role/{id}','Admin\RoleProfileController@assignRole');
+Route::resource('roleProfile','Admin\RoleProfileController');
 Route::post('user/login','UserController@store');
 
