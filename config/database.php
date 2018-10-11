e2d1948649dd6383b503b8dfea4330dd51dccc76
fatuma.mkima <fettymkima@gmail.com>
2018-10-11 20:00:54 +0300

    cofin.db

--- config/database.php
+++ config/database.php
@@ -55,9 +55,7 @@ return [
             'options'   => [
                 \PDO::ATTR_EMULATE_PREPARES => true
             ]
-            'options'   => [
-                \PDO::ATTR_EMULATE_PREPARES => true
-            ],
+          
         ],
 
         'pgsql' => [
