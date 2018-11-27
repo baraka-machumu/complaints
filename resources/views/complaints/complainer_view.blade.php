b9fa1678b83c24f96b5f23883ae6e55ebe6bb6e2
fatuma.mkima <fettymkima@gmail.com>
2018-11-27 15:31:34 +0300

    font size changes

--- resources/views/complaints/complainer_view.blade.php
+++ resources/views/complaints/complainer_view.blade.php
@@ -12,63 +12,65 @@
 
             <div class="col-md-3" id="hint">
 
-                <h4 style="text-align: center">Hints</h4>
+                <h4 style="text-align: center; font-size: 16px;">Hints</h4>
 
                 <div style="background-color: #F4F4F4">
 
-                <ul>
-                    <li>
-                        If you are making a follow up <a href="{{url('complaints/followups')}}">click here</a>
-                    </li>
-                    <li>
-                        All fields marked (*) are mandatory
-                    </li>
-                </ul>
+                    <ul>
+                        <li style="font-size: 16px">
+                            If you are making a follow up <a style="font-size: 16px" href="{{url('complaints/followups')}}">click here</a>
+                        </li>
+                        {{--<li style="font-size: 16px">--}}
+                            {{--All fields marked (*) are mandatory--}}
+                        {{--</li>--}}
+                        <li style="font-size: 16px">Simu: +255 787 498 475, +255 22 2761683-4, +255 22 2761689, </li>
+                        <li style="font-size: 16px">Barua pepe: info@ssra.go.tz</li>
+                    </ul>
                 </div>
 
             </div>
             <div class="col-md-4">
                 <h4>Taarifa Binafsi/Personal Information</h4>
                 <div class="form-group">
-                    <label for="first_name">Jina la Kwanza/First Name:</label>
+                    <label for="first_name" style="font-size: 16px">Jina la Kwanza/First Name:</label>
                     <input type="text" class="form-control" id="first_name" name="first_name" >
                 </div>
 
 
                 <div class="form-group">
-                    <label for="middle_name">Jina La Kati/Middle Name:</label>
+                    <label for="middle_name" style="font-size: 16px">Jina La Kati/Middle Name:</label>
                     <input type="text" class="form-control" id="middle_name" name="middle_name">
                 </div>
 
-                <div class="form-group">
+                <div class="form-group" style="font-size: 16px">
                     <label for="surname">Jina la ukoo/Last Name:</label>
                     <input type="text" class="form-control" id="surname" name="surname">
                 </div>
 
 
                 <div class="form-group">
-                    <label for="postal">S.L.P/P.O Box:</label>
+                    <label for="postal" style="font-size: 16px">S.L.P/P.O Box:</label>
                     <input type="text" class="form-control" id="postal" name="postal">
                 </div>
                 <div class="form-group">
-                    <label for="residence">Makazi/Residence:</label>
+                    <label for="residence" style="font-size: 16px">Makazi/Residence:</label>
                     <input type="text" class="form-control" id="residence" name="residence">
                 </div>
 
                 <div class="form-group">
-                    <label for="phone">Simu/Phone Number:</label>
+                    <label for="phone" style="font-size: 16px">Simu/Phone Number:</label>
                     <input type="text" class="form-control" id="phone" name="phone">
                 </div>
 
                 <div class="form-group">
-                    <label for="email">Barua Pepe/Email:</label>
+                    <label for="email" style="font-size: 16px">Barua Pepe/Email:</label>
                     <input type="email" class="form-control" id="email" name="email">
                 </div>
             </div>
             <div class="col-md-5">
                 <h4>Social Security Details</h4>
                 <div class="form-group">
-                    <label for="ssinfo">Social Security Status:</label>
+                    <label for="ssinfo" style="font-size: 16px">Social Security Status:</label>
                     <select class="form-control" id="ssinfo" name="ssinfo_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($membership_status as $status)
                             <option value="{{$status['ssinfo_id']}}">{{$status['ssinfo_name']}}</option>
@@ -76,7 +78,7 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="scheme">Scheme:</label>
+                    <label for="scheme" style="font-size: 16px">Scheme:</label>
                     <select class="form-control" id="scheme" name="scheme_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($all_scheme_types as $all_scheme_type)
                             <option value="{{$all_scheme_type['scheme_id']}}">{{$all_scheme_type['scheme_name']}}</option>
@@ -84,11 +86,11 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="ssno">Membership Number:</label>
+                    <label for="ssno" style="font-size: 16px">Membership Number:</label>
                     <input type="text" class="form-control" id="ssno" name="ssno" >
                 </div>
                 <div class="form-group">
-                    <label for="employer">Employer:</label>
+                    <label for="employer" style="font-size: 16px">Employer:</label>
                     <input type="text" class="form-control" id="employer" name="employer" >
                 </div>
 
@@ -96,7 +98,7 @@
                 <h4>Complaints Details</h4>
 
                 <div class="form-group">
-                    <label for="complaint_type">Complaint Type</label>
+                    <label for="complaint_type" style="font-size: 16px">Complaint Type</label>
                     <select class="form-control" id="complaint_type" name="complaint_type_id"  required>
                         @foreach($complaintsTypes as $complaintsType)
                             <option value="{{$complaintsType['complaint_type_id']}}">{{$complaintsType['complaint_type_name']}}</option>
@@ -105,7 +107,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="complaint_count_id">Individual or Group Complaint:</label>
+                    <label for="complaint_count_id" style="font-size: 16px">Individual or Group Complaint:</label>
                     <select class="form-control" id="complaint_count_id" name="complaint_count_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($groupcomplaints as $groupcomplaint)
                             <option value="{{$groupcomplaint['complaint_count_id']}}">{{$groupcomplaint['complaint_type']}}</option>
@@ -114,12 +116,12 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="complaints">Lalamiko/Complaint</label>
+                    <label for="complaints" style="font-size: 16px">Lalamiko/Complaint</label>
                     <textarea type="text" class="form-control" id="complaint" name="complaint" ></textarea>
                 </div>
 
                 <div class="form-group">
-                    <label for="validator">Enter This Text <span id="verify_text" style="font-weight: bold; font-size: 20px; color:#3C8DBC"></span></label>
+                    <label for="validator" style="font-size: 16px">Enter This Text <span id="verify_text" style="font-weight: bold; font-size: 20px; color:#3C8DBC"></span></label>
                     <input type="text" class="form-control" id="validator" name="validator" >
                 </div>
 
@@ -151,7 +153,7 @@
         #hint{background-color: #F4F4F4;}
 
     </style>
-    @stop
+@stop
 
 @section('js')
     <script>
--- resources/views/complaints/create.blade.php
+++ resources/views/complaints/create.blade.php
@@ -21,44 +21,44 @@
             <div class="col-md-4">
                 <h4>Taarifa Binafsi/Personal Information</h4>
                 <div class="form-group">
-                    <label for="first_name">Jina la Kwanza/First Name:</label>
+                    <label for="first_name" style="font-size: 16px">Jina la Kwanza/First Name:</label>
                     <input type="text" class="form-control" id="first_name" name="first_name" >
                 </div>
 
                 <div class="form-group">
-                    <label for="middle_name">Jina La Kati/Middle Name:</label>
+                    <label for="middle_name" style="font-size: 16px">Jina La Kati/Middle Name:</label>
                     <input type="text" class="form-control" id="middle_name" name="middle_name">
                 </div>
 
                 <div class="form-group">
-                    <label for="surname">Jina la ukoo/Last Name:</label>
+                    <label for="surname" style="font-size: 16px">Jina la ukoo/Last Name:</label>
                     <input type="text" class="form-control" id="surname" name="surname">
                 </div>
 
 
                 <div class="form-group">
-                    <label for="postal">S.L.P/P.O Box:</label>
+                    <label for="postal" style="font-size: 16px">S.L.P/P.O Box:</label>
                     <input type="text" class="form-control" id="postal" name="postal">
                 </div>
                 <div class="form-group">
-                    <label for="residence">Makazi/Residence:</label>
+                    <label for="residence" style="font-size: 16px">Makazi/Residence:</label>
                     <input type="text" class="form-control" id="residence" name="residence">
                 </div>
 
                 <div class="form-group">
-                    <label for="phone">Simu/Phone Number:</label>
+                    <label for="phone" style="font-size: 16px">Simu/Phone Number:</label>
                     <input type="text" class="form-control"  id="phone" name="phone" style="width:315px">
                 </div>
 
                 <div class="form-group">
-                    <label for="email">Barua Pepe/Email:</label>
+                    <label for="email" style="font-size: 16px">Barua Pepe/Email:</label>
                     <input type="email" class="form-control" id="email" name="email">
                 </div>
             </div>
             <div class="col-md-6">
                 <h4>Social Security Details</h4>
                 <div class="form-group">
-                    <label for="ssinfo">Social Security Status:</label>
+                    <label for="ssinfo" style="font-size: 16px">Social Security Status:</label>
                     <select class="form-control" id="ssinfo" name="ssinfo_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($membership_status as $status)
                             <option value="{{$status['ssinfo_id']}}">{{$status['ssinfo_name']}}</option>
@@ -66,7 +66,7 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="scheme">Scheme:</label>
+                    <label for="scheme" style="font-size: 16px">Scheme:</label>
                     <select class="form-control" id="scheme" name="scheme_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($all_scheme_types as $all_scheme_type)
                             <option value="{{$all_scheme_type['scheme_id']}}">{{$all_scheme_type['scheme_name']}}</option>
@@ -74,11 +74,11 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="ssno">Membership Number:</label>
+                    <label for="ssno" style="font-size: 16px">Membership Number:</label>
                     <input type="text" class="form-control" id="ssno" name="ssno" >
                 </div>
                 <div class="form-group">
-                    <label for="employer">Employer:</label>
+                    <label for="employer" style="font-size: 16px">Employer:</label>
                     <input type="text" class="form-control" id="employer" name="employer" >
                 </div>
 
@@ -86,7 +86,7 @@
                 <h4>Complaints Details</h4>
 
                 <div class="form-group">
-                    <label for="complaint_rec_mode_id">Complaint Receive Mode:</label>
+                    <label for="complaint_rec_mode_id" style="font-size: 16px">Complaint Receive Mode:</label>
                     <select class="form-control" id="complaint_rec_mode_id" name="complaint_rec_mode_id"  style="height: 35px; font-size: 14px" required>
                         @foreach($rec_modes as $rec_mode)
                             <option value="{{$rec_mode['complaint_rec_mode_id']}}">{{$rec_mode['complaint_rec_mode_name']}}</option>
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="complaint_type">Complaint Type</label>
+                    <label for="complaint_type" style="font-size: 16px">Complaint Type</label>
                     <select class="form-control" id="complaint_type" name="complaint_type_id"  required>
                         @foreach($complaintsTypes as $complaintsType)
                             <option value="{{$complaintsType['complaint_type_id']}}">{{$complaintsType['complaint_type_name']}}</option>
@@ -104,7 +104,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="complaints">Lalamiko/Complaint</label>
+                    <label for="complaints" style="font-size: 16px">Lalamiko/Complaint</label>
                     <textarea type="text" class="form-control" id="complaint" name="complaint" ></textarea>
                 </div>
 
